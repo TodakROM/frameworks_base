9280623c5091d2386fddc12b94329a8bd517a890
xlm03lx <55821303+xlm03lx@users.noreply.github.com>
2020-03-11 13:59:22 +0800

    Camera button support
    This patch adds:
    - Use camera button as wake key
    - Use focus button as peek and wake key
    - Use camera button to launch (secure) camera

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7437,7 +7437,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // Check if screen is fully on before letting the device go to sleep
                     if (mScreenOnFully && mIsFocusPressed) {
                         mPowerManager.goToSleep(SystemClock.uptimeMillis());
-                    } else if (!interactive && mCameraSleepOnRelease) {
+                    } else if (mCameraSleepOnRelease) {
                         mFocusReleasedGoToSleep = true;
                     }
                     mIsFocusPressed = false;
